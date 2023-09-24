5c6f5aaa310d163b63c38bc0487ed24e6880d5e3
Babar Khan <52751757+bobiiii@users.noreply.github.com>
2023-09-24 05:33:23 -0700

    #

--- src/config/mongo.connect.js
+++ src/config/mongo.connect.js
@@ -6,7 +6,7 @@ const connectMongoDB = async () => {
   try {
     mongooose.set('strictQuery', true);
     if (env.NODE_ENV === 'development') {
-      await mongooose.connect('mongodb+srv://babarkhan:EDashboard@dashboarddb.cmk8ncf.mongodb.net/?retryWrites=true&w=majority', { useNewurlParser: true, useUnifiedTopology: true });
+      await mongooose.connect('mongodb+srv://babarkhan:EDashboard@dashboarddb.cmk8ncf.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
     } else {
       await mongooose.connect(env.MONGO_URI, { useNewurlParser: true, useUnifiedTopology: true });
     }
