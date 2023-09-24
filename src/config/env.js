0966692ee218578ff2f43b6a7912566fa606b21c
Babar Khan <52751757+bobiiii@users.noreply.github.com>
2023-09-24 05:42:02 -0700

    #

--- src/config/env.js
+++ src/config/env.js
@@ -6,5 +6,6 @@ module.exports = {
   APP_HOST: process.env.APP_HOST,
   NODE_ENV: process.env.NODE_ENV,
   MONGO_URI_TEST: process.env.MONGO_URI_TEST,
+  MONGO_URI: process.env.MONGO_URI,
   SECRET_KEY: process.env.SECRET_KEY,
 };
